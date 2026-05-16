559a614e6e588c81bee91e024d188bfe5a3beff5
tuanne <ngotuan16072005@gmail.com>
2026-05-16 09:51:32 +0700

    user.py

---
+++ web/models/user.py
@@ -0,0 +1,25 @@
+from app.extensions import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(15))
+    email = db.Column(db.String(100), unique=True)
+    role = db.Column(db.Enum("admin", "doctor", "staff"), nullable=False)
+    status = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def get_id(self):
+        return str(self.user_id)
+
+    def set_password(self, raw_password):
+        self.password = generate_password_hash(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password_hash(self.password, raw_password)
